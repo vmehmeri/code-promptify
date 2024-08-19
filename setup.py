1ea1fe943edf1f052b9971268ef35f921f2bc7fa
Victor Dantas <vmehmeri@hotmail.com>
2024-08-19 20:02:40 +0200

    Add version number metadata

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="code-promptify",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=required,
     entry_points={
