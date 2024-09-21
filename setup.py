356f471fc38958f56d54d338e3143abc82e9df8b
Victor Dantas <vmehmeri@hotmail.com>
2024-09-21 08:44:57 +0200

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="code-promptify",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(),
     install_requires=required,
     entry_points={
